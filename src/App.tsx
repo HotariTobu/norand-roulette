80f0c0a35461ca1eb03d012f77bb1aeb115841a8
HotariTobu <hotari24tools@gmail.com>
2024-07-24 16:16:44 +0900

    fix: layout

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
         <div className="text-lg md:text-xl">{t('items.header')}</div>
         <Textarea className="md:text-lg" rows={8} value={itemText} onChange={e => setItemText(e.target.value)} />
       </div>
-      <div className="w-9/12 m-auto select-none overflow-clip md:row-span-3 relative">
+      <div className="w-3/4 md:max-w-[90vh] m-auto select-none overflow-clip relative md:row-span-2">
         <Accelerator acceleration={acceleration} attenuation={attenuation} >
           <Wheel items={items} />
         </Accelerator>
@@ -62,7 +62,7 @@ function App() {
           <Slider min={0.0001} max={0.01} step={0.00001} value={[attenuation]} onValueChange={v => setAttenuation(v[0])} />
         </div>
       </div>
-      <div className="m-2 space-y-2 md:space-y-4">
+      <div className="m-2 space-y-2 md:space-y-4 md:col-span-2">
         <div className="text-lg md:text-xl">{t('how-to-use.header')}</div>
         <div>{t('how-to-use.body')}</div>
       </div>
